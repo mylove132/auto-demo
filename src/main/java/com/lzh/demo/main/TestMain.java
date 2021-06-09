c6478470a11f74add38ed25fb189474294a9070b
unknown <liuzhanhui@360buyad.local>
2021-06-09 17:26:45 +0800

    modify main func

--- src/main/java/com/lzh/demo/main/TestMain.java
+++ src/main/java/com/lzh/demo/main/TestMain.java
@@ -1,6 +1,7 @@
 package com.lzh.demo.main;
 import org.testng.TestNG;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestMain {
@@ -8,7 +9,11 @@ public class TestMain {
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
         List<String> testngXmlList = new ArrayList<>();
-        testngXmlList.add("D:\\liuzhanhui\\Downloads\\auto-demo\\testng.xml");
+        if (args.length > 0) {
+            testngXmlList.addAll(Arrays.asList(args));
+        } else {
+            testngXmlList.add("/var/jenkins_home/testxml/testng.xml");
+        }
         testNG.setTestSuites(testngXmlList);
         testNG.run();
     }
